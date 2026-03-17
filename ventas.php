7649cf561d5d880fb5fa4ba8ac1132e3fe92c622
lunlara179-collab <lunlara179@gmail.com>
2026-03-17 10:12:16 -0600

    Create ventas.php

---
+++ ventas.php
@@ -0,0 +1,9 @@
+<h2>Registro de Ventas</h2>
+
+<form action="guardar_venta.php" method="POST">
+    <input type="text" name="producto" placeholder="Producto"><br><br>
+    <input type="number" name="cantidad" placeholder="Cantidad"><br><br>
+    <input type="number" name="precio" placeholder="Precio"><br><br>
+    <input type="date" name="fecha"><br><br>
+    <button type="submit">Guardar</button>
+</form>
