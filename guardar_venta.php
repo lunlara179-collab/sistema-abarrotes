c9bb32efc41a1d5c28cdabea56a632e2d82f4ad1
FerRomeroD <161257293+FerRomeroD@users.noreply.github.com>
2026-03-18 21:08:45 -0600

    Update guardar_venta.php

--- guardar_venta.php
+++ guardar_venta.php
@@ -1,12 +1,19 @@
 <?php
+include 'conexión.php';
 $producto = $_POST['producto'];
 $cantidad = $_POST['cantidad'];
 $precio = $_POST['precio'];
 $fecha = $_POST['fecha'];
+if ($cantidad > 0 && $precio > 0) {
+    $sql = "INSERT INTO ventas (producto, cantidad, precio_unitario, fecha) 
+            VALUES ('$producto', $cantidad, $precio, '$fecha')";
 
-echo "<h3>Venta registrada</h3>";
-echo "Producto: " . $producto . "<br>";
-echo "Cantidad: " . $cantidad . "<br>";
-echo "Precio: " . $precio . "<br>";
-echo "Fecha: " . $fecha;
+    if (mysqli_query($conn, $sql)) {
+        echo "<script>alert('Venta registrada con éxito'); window.location='ventas.php';</script>";
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
+} else {
+    echo "<script>alert('Los datos deben ser positivos'); window.history.back();</script>";
+}
 ?>
