2ca375bb9cca4a58102de692e486957d549a9bde
lunlara179-collab <lunlara179@gmail.com>
2026-03-22 22:46:57 -0600

    Remove validation for empty fields in guardar.php
    Removed basic validation for required fields in guardar.php.

--- ventas/guardar.php
+++ ventas/guardar.php
@@ -1,12 +1,6 @@
 <?php
 include("../config/conexion.php");
 
-// Validación básica
-if(empty($_POST['producto']) || empty($_POST['cantidad']) || empty($_POST['precio']) || empty($_POST['fecha'])){
-    echo "Todos los campos son obligatorios";
-    exit;
-}
-
 $producto = $_POST['producto'];
 $cantidad = $_POST['cantidad'];
 $precio = $_POST['precio'];
@@ -16,8 +10,7 @@ $sql = "INSERT INTO ventas (producto, cantidad, precio, fecha)
 VALUES ('$producto', '$cantidad', '$precio', '$fecha')";
 
 if ($conn->query($sql)) {
-    echo "Guardado correctamente <br>";
-    echo "<a href='formulario.php'>Regresar</a>";
+    echo "Guardado correctamente";
 } else {
     echo "Error: " . $conn->error;
 }
