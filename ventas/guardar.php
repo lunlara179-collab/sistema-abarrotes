d21315e3b4acdf7d63b4b4946684813c42839ac6
lunlara179-collab <lunlara179@gmail.com>
2026-03-22 22:42:04 -0600

    Update guardar.php

--- ventas/guardar.php
+++ ventas/guardar.php
@@ -1,8 +1,8 @@
 <?php
-include("../conexion.php");
+include("../config/conexion.php");
 
-// Validar datos
-if (empty($_POST['producto']) || empty($_POST['cantidad']) || empty($_POST['precio']) || empty($_POST['fecha'])) {
+// Validación básica
+if(empty($_POST['producto']) || empty($_POST['cantidad']) || empty($_POST['precio']) || empty($_POST['fecha'])){
     echo "Todos los campos son obligatorios";
     exit;
 }
@@ -12,12 +12,12 @@ $cantidad = $_POST['cantidad'];
 $precio = $_POST['precio'];
 $fecha = $_POST['fecha'];
 
-// Insertar en la base de datos
 $sql = "INSERT INTO ventas (producto, cantidad, precio, fecha)
-        VALUES ('$producto', '$cantidad', '$precio', '$fecha')";
+VALUES ('$producto', '$cantidad', '$precio', '$fecha')";
 
-if ($conn->query($sql) === TRUE) {
-    echo "Venta guardada correctamente";
+if ($conn->query($sql)) {
+    echo "Guardado correctamente <br>";
+    echo "<a href='formulario.php'>Regresar</a>";
 } else {
     echo "Error: " . $conn->error;
 }
