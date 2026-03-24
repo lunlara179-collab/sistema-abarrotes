db91cb1d319fc675c861320ec354f1e178fad1fc
fabiolahernandez23 <161089809+fabiolahernandez354@users.noreply.github.com>
2026-03-23 23:17:26 -0600

    Create registrar.php

---
+++ ventas/registrar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registrar Venta</title>
+</head>
+<body>
+
+<h2>Registrar Venta</h2>
+
+<form action="guardar.php" method="POST">
+    Producto: <input type="text" name="producto"><br><br>
+    Cantidad: <input type="number" name="cantidad"><br><br>
+    Precio: <input type="number" step="0.01" name="precio"><br><br>
+
+    <button type="submit">Guardar</button>
+</form>
+
+</body>
+</html>
