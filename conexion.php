82297aee2b5ae4998e7b8fce3e56ba463a304fef
lunlara179-collab <lunlara179@gmail.com>
2026-03-24 22:54:56 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,15 @@
 <?php
 try {
     $ruta = __DIR__ . "/data/abarrotes.db";
+
+    // Verifica si el archivo existe
+    if (!file_exists($ruta)) {
+        die("NO EXISTE LA BASE EN: " . $ruta);
+    }
+
     $conn = new PDO("sqlite:$ruta");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
