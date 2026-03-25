ec9938a3feaee1e9d5b6b82a35170df1e3827742
lunlara179-collab <lunlara179@gmail.com>
2026-03-24 22:35:41 -0600

    Add form for registering sales in registrar.php

--- ventas/registrar.php
+++ ventas/registrar.php
@@ -78,4 +78,27 @@ label {
 </style>
 
 </body>
+</html> <!DOCTYPE html>
+<html>
+<head>
+    <title>Registrar Venta</title>
+</head>
+
+<body>
+
+<h2>Registrar Venta</h2>
+
+<form action="guardar.php" method="POST">
+    <input type="text" name="producto" placeholder="Producto" required><br><br>
+
+    <input type="number" name="cantidad" placeholder="Cantidad" required><br><br>
+
+    <input type="number" step="0.01" name="precio" placeholder="Precio" required><br><br>
+
+    <button type="submit">Guardar</button>
+</form>
+
+<a href="../index.php">Volver</a>
+
+</body>
 </html>
