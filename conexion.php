9f7d5721c1ed7713f987f44c9404eb374b53a94a
lunlara179-collab <lunlara179@gmail.com>
2026-03-24 22:48:41 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,6 +1,7 @@
 <?php
 try {
-    $conn = new PDO("sqlite:data/abarrotes.db");
+    $ruta = __DIR__ . "/data/abarrotes.db";
+    $conn = new PDO("sqlite:$ruta");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
