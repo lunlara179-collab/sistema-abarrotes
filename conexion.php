0236801be04590bde1418289a71f3d585992d08d
lunlara179-collab <lunlara179@gmail.com>
2026-03-25 10:22:05 -0600

    Remove file existence check for database connection
    Removed the check for the existence of the database file.

--- conexion.php
+++ conexion.php
@@ -2,11 +2,6 @@
 try {
     $ruta = __DIR__ . "/data/abarrotes.db";
 
-    // Verifica si el archivo existe
-    if (!file_exists($ruta)) {
-        die("NO EXISTE LA BASE EN: " . $ruta);
-    }
-
     $conn = new PDO("sqlite:$ruta");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
