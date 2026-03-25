f166464b263707d7bbef8ff8be0c38e820f7d6ce
lunlara179-collab <lunlara179@gmail.com>
2026-03-24 22:06:59 -0600

    Change database connection from MySQL to SQLite

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $conn = new PDO("mysql:host=localhost;dbname=sig", "root", "");
+    $conn = new PDO("sqlite:data/abarrotes.db");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
